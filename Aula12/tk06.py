e8a2ac68bb4c674eb3fde789f63275cb01701f71
Ana Maísa <d2021002575@unifei.edu.br>
2022-11-08 14:28:55 -0300

    Exercício TK06

--- Aula12/tk06.py
+++ Aula12/tk06.py
@@ -6,13 +6,19 @@ class GUI:
       self.janela = tk.Tk()
       self.frame1 = tk.Frame(self.janela)
       self.frame2 = tk.Frame(self.janela)
+      self.frame3 = tk.Frame(self.janela)
       self.frame1.pack()
       self.frame2.pack()
+      self.frame3.pack()
       
-      self.labelInfo = tk.Label(self.frame1,text="Digite algo:")
-      self.labelResult = tk.Label(self.frame2,text="Nada")
-      self.labelInfo.pack(side="left")
-      self.labelResult.pack(side="left")
+      self.labelInfo1 = tk.Label(self.frame1,text="Digite algo 1:")
+      self.labelInfo2 = tk.Label(self.frame2,text="Digite algo 2:")
+      self.labelResult1 = tk.Label(self.frame3,text="Nada")
+      self.labelResult2 = tk.Label(self.frame3,text="Nada")
+      self.labelInfo1.pack(side="left")
+      self.labelInfo2.pack(side="left")
+      self.labelResult1.pack(side="left")
+      self.labelResult2.pack(side="left")
       
       self.buttonSubmit = tk.Button(self.janela,text="Enter", \
                                     command=self.submit)      
@@ -23,19 +29,32 @@ class GUI:
       self.buttonClear.pack(side="left")
       
       # Criando o objeto Entry
-      self.inputText = tk.Entry(self.frame1, width=20)
-      self.inputText.pack(side="left")
+      self.inputText1 = tk.Entry(self.frame1, width=20)
+      self.inputText1.pack(side="left")
+      
+      self.inputText2 = tk.Entry(self.frame2, width=20)
+      self.inputText2.pack(side="left")
       
       self.janela.mainloop()
    
    # Criando as funções de callback
    def submit(self):
       # O texto pode ser recuperado do com o comando get()
-      self.labelResult["text"] = self.inputText.get()
+      self.labelResult1["text"] = self.inputText1.get()
+      self.labelResult2["text"] = self.inputText2.get()
+
+      if (self.labelResult1["text"] == ""):
+         self.labelResult1["text"] = "Nada"
+      
+      if (self.labelResult2["text"] == ""):
+         self.labelResult2["text"] = "Nada"
       
    def clear(self):
-      self.inputText.delete(0, len(self.inputText.get()))
-      self.labelResult["text"] = "Nada"
+      self.inputText1.delete(0, len(self.inputText1.get()))
+      self.labelResult1["text"] = "Nada"
+
+      self.inputText2.delete(0, len(self.inputText2.get()))
+      self.labelResult2["text"] = "Nada"
    
 def main():
    GUI()
